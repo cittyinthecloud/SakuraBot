ac83400b421d91203ff9c908c77bbb4da6300410
famous1622 <gindelberger.2@wright.edu>
2019-06-24 16:40:06 -0400

    Session 1.5

--- main.py
+++ main.py
@@ -12,18 +12,21 @@ logging.basicConfig(level=logging.INFO)
 
 bot: Bot
 
-
 async def asyncinit():
-    await Tortoise.init(db_url="sqlite://db.sqlite3", modules=dict(models=["models"]))
+    await Tortoise.init(
+        db_url="sqlite://db.sqlite3",
+        modules=dict(models=["models"])
+    )
     await Tortoise.generate_schemas()
-
 
 if __name__ == "__main__":
     bot = commands.Bot(command_prefix=commands.when_mentioned_or("&"))
-    bot.loop.create_task(asyncinit())
+    bot.loop.run_until_complete(asyncinit())
     for ext in init_extensions:
         try:
             bot.load_extension(extension)
         except Exception as e:
             print(f"Failed to load extension {ext}", file=sys.stderr)
             traceback.print_exc()
+
+    
